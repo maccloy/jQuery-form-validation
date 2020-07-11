bc41b80f7a5c580672084bc8281c916bf828be22
Hamish MacCloy <nick_cloy@hotmail.com>
2020-07-11 10:34:43 +0100

    added error messages

--- javascript.js
+++ javascript.js
@@ -1,44 +1,78 @@
 
-
-
 $(document).ready(function(){
 
     $("#name").blur(function(){
 
-
         var formData = {
-            'name'              : $('input[name=name]').val(),
+            'data'              : $('input[name=name]').val(),
+            'type'              : 'string',
+            'minLength'         : '5',
+            'maxLength'         : '10',
+            'field'             : 'Name',
         };
-;
 
-        $.ajax({type: "GET", url: "submit.php", data: formData, success: function(result){
-
+        $.ajax({type: "POST", url: "submit.php", data: formData, success: function(result){
+            //alert(result)
             if (result == "success") {
                 $('#name').addClass("is-valid");
                 $('#name').removeClass("is-invalid");
+                $('#nameError').text("");
             } else {
                 $('#name').addClass("is-invalid");
                 $('#name').removeClass("is-valid");
+
+                $('#nameError').text(result);
             }
         }});
     });
 
 
+    $("#email").blur(function(){
 
-    /*
+        var formData = {
+            'data'              : $('input[name=email]').val(),
+            'type'              : 'email',
+            'field'             : 'Email',
+        };
 
-$.ajax({
-    type: "GET",
-    url: 'test.php',
-    data: {name: 'Wayne'},
-    success: function(data){
-        alert(data);
-    }
-});
+        $.ajax({type: "POST", url: "submit.php", data: formData, success: function(result){
+            //alert(result)
+            if (result == "success") {
+                $('#email').addClass("is-valid");
+                $('#email').removeClass("is-invalid");
+                $('#emailError').text("");
+            } else {
+                $('#email').addClass("is-invalid");
+                $('#email').removeClass("is-valid");
+                $('#emailError').text(result);
 
-*/
+            }
+        }});
+    });
 
 
+    $("#age").blur(function(){
 
+        var formData = {
+            'data'              : $('input[name=age]').val(),
+            'type'              : 'int',
+            'min'               : '5',
+            'max'               : '100',
+            'field'             : 'Age',
+        };
+
+        $.ajax({type: "POST", url: "submit.php", data: formData, success: function(result){
+            //alert(result)
+            if (result == "success") {
+                $('#age').addClass("is-valid");
+                $('#age').removeClass("is-invalid");
+                $('#ageError').text("");
+            } else {
+                $('#age').addClass("is-invalid");
+                $('#age').removeClass("is-valid");
+                $('#ageError').text(result);
+            }
+        }});
+    });
 
 })
--- submit.php
+++ submit.php
@@ -1,10 +1,54 @@
 <?php
-$val = implode(", ", $_GET);
+$data = $_POST["data"];
+$field = $_POST["field"];
+$type = $_POST["type"];
 
-if ($val == "test") {
-    echo "success";
-} else {
-    echo "$val";
+$result = "success";
+
+switch ($type) {
+    case "string":
+        if (strlen($data) < $_POST["minLength"]) {
+            $result = "$field must be 5 charcters or more";
+        }
+        
+        if (strlen($data) > $_POST["maxLength"]) {
+            $result = "$field must be 10 charcters or fewer";
+        }
+    break;
+    
+    case "email":
+        if (!filter_var($data, FILTER_VALIDATE_EMAIL)) {
+            $result = "Not a properly formatted email address";
+        }
+    break;
+
+    case "int":
+        if (!filter_var($data, FILTER_VALIDATE_INT)) {
+            $result = "Not a whole number";
+        } else {
+
+            $min = $_POST['min'];
+            $max = $_POST["max"];
+
+            if ($data < $min) {
+                
+                $result = "Age must be $min or over";
+            }
+
+            if ($data > $max) {
+                $result = "Age must be $max or under";
+            }
+
+        }
+    break;
+
 }
+
+
+echo $result;
+
+
+//echo $type;
+
 
 ?>
